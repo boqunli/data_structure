2163b9933e203852b9bd85085469233b3c1dec17
boqun.li <boqun.li@sjtu.edu.cn>
2022-01-27 15:41:08 +0800

    structure

--- README.md
+++ README.md
@@ -3,4 +3,33 @@
 ### Sorting alogorithm
 
 + bubble_sort
-+ 
++ selection_sort
++
++
++
+
+### Linear data structure
++ linked_list
++ doubly_linked_list
++ queue
++ deque
++ stack
+
+### Tree
++
++
++
+
+### Graph
+
+
+
+### Hash table
+
+
+
+
+
+
+
+
---
+++ sort/selection_sort.py
@@ -0,0 +1,32 @@
+'''
+selection Sort
+O(n^2)
+
+repeat (numOfElements - 1) times
+  set the first unsorted element as the minimum
+  for each of the unsorted elements
+    if element < currentMinimum
+      set element as new minimum
+  swap minimum with first unsorted position
+'''
+
+from json.tool import main
+import random
+
+
+def selection_sort(array: list) -> list:
+    
+    pass
+    # return array
+
+
+def selection_sort_test():
+    low, high = 0, 100
+    array = [random.randint(low, high) for _ in range(0, 10)]
+    print(array)
+    selection_sort(array)
+    print(array)
+
+if __name__ == '__main__':
+    selection_sort_test()
+
